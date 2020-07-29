e9a4cc2f1a9693a0945b31acdb1f6aa91833dbef
Guillaume C <cguillaume4@gmail.com>
2020-07-29 22:06:29 +0200

    Security: remove secret key

--- myinfo/settings.py
+++ myinfo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hmwqr$yaper9#@ep8sgq(2as1-$rpzwu**ol+pnu67sy=0ztov'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
